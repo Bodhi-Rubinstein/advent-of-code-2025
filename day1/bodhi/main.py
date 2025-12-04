b16d1e3c96ce5ac4bb36e276aa80a02678f51a21
Bodhi-Rubinstein <bodhirubinstein@gmail.com>
2025-12-04 12:01:06 -0700

    Finished second star day1

--- day1/bodhi/main.py
+++ day1/bodhi/main.py
@@ -2,29 +2,30 @@ import pandas as pd
 
 count = 0
 dial = 50
-last = dial
 
 turns = pd.read_csv("./puzzle1.txt")
 
 for turn in turns["dial"]:
     dir = turn[:1]
     num = int(turn[1:])
+    last = dial
+    flag = True
 
     if (dir == "R"): dial += num
     else: dial -= num
 
-    #if (dial <= 0): count += (abs(dial)//100 + 1)
-    #if (dial >= 100): count += (dial//100)
+    if (abs(dial) > 100): 
+        count += abs(dial)//100
+        flag = False
+    if (dial < 0 and last > 0): count += 1
 
-    if (abs(dial) > 100): count += abs(dial)//100
-    elif (dial < 0 and last > 0): count += 1
-
-    print ("Turn: " + turn + ", Dial: " + str(dial))
+    #print ("Turn: " + turn + ", Dial Total: " + str(dial))
 
     dial = dial % 100
-    last = dial
+
+    #print("Dial adjusted: " + str(dial))
     
-    if (dial == 0): count += 1
-    print ("Count: " + str(count))
+    if (dial == 0 and flag): count += 1
+    #print ("Count: " + str(count))
 
 print(count)
